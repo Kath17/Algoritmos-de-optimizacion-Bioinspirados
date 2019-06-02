f12e51c39536889b585bcb576eb521e9704ef1d3
Katherine <kunapilco@unsa.edu.pe>
2019-06-01 23:48:54 -0500

    laboratorio 13

---
+++ Lab13/Lab13.py
@@ -0,0 +1,15 @@
+import random
+import math
+import os
+import numpy as np
+import pandas as pd
+import sys
+import copy
+
+orig_stdout = sys.stdout
+f = open('Lab13.txt', 'w')
+sys.stdout = f
+
+Main()
+
+
